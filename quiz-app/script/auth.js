6df267882ed26b1e6c0a020813984b23bfe531ff
Abed-Aldani <abedaldani2@gmail.com>
2025-04-20 23:38:34 +0300

    Registering a new user, and rejecting existed users

--- quiz-app/script/auth.js
+++ quiz-app/script/auth.js
@@ -8,3 +8,26 @@ document.getElementById('registerTab').addEventListener('click', () => {
   document.getElementById('registerForm').classList.remove('hidden');
   document.getElementById('loginForm').classList.add('hidden');
 });
+
+
+
+
+function register() {
+  const email = document.getElementById('registerEmail').value;
+  const password = document.getElementById('registerPassword').value;
+
+  if (!email || !password) return alert("Please fill all fields.");
+
+  let users = JSON.parse(localStorage.getItem('users')) || [];
+
+  if (users.find(user => user.email === email)) {
+    return alert("User already exists!");
+  }
+
+  users.push({ email, password, scores: [] });
+  localStorage.setItem('users', JSON.stringify(users));
+
+  alert("Registration successful. You can now login.");
+  document.getElementById('loginTab').click();
+}
+
