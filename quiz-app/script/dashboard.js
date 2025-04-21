2237fe807ac933e3525d5247c0b576e4c6182dfc
Abed-Aldani <abedaldani2@gmail.com>
2025-04-21 21:25:29 +0300

    Creating a table to put all the gotten info in

--- quiz-app/script/dashboard.js
+++ quiz-app/script/dashboard.js
@@ -10,3 +10,20 @@ const users = JSON.parse(localStorage.getItem("users")) || [];
 const quizzes = JSON.parse(localStorage.getItem("quizzes")) || [];
 
 const userTable = document.getElementById("userTable");
+
+
+
+let tableHTML = "<table><tr><th>Email</th><th>Scores</th></tr>";
+
+users.forEach(user => {
+  const scoreList = (user.scores || [])
+    .map(score => {
+      const quizTitle = quizzes.find(q => q.id === score.quizId)?.title || "Unknown Quiz";
+      return `${quizTitle}: ${score.score}`;
+    }).join("<br>");
+
+  tableHTML += `<tr><td>${user.email}</td><td>${scoreList || "No scores yet"}</td></tr>`;
+});
+
+tableHTML += "</table>";
+userTable.innerHTML = tableHTML;
