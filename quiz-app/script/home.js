ad6191422436a30ae0370621c343f8fffa8d20c1
Abed-Aldani <abedaldani2@gmail.com>
2025-04-21 17:32:38 +0300

    Adding Samples quizzes

--- quiz-app/script/home.js
+++ quiz-app/script/home.js
@@ -3,3 +3,9 @@ const currentUser = localStorage.getItem("currentUser") || "User";
 
 const username = currentUser.split("@")[0];
 document.getElementById("userEmail").textContent = username;
+
+const sampleQuizzes = [
+  { id: 1, title: "MATH" },
+  { id: 2, title: "Physics" },
+  { id: 3, title: "Chemistry" }
+];
