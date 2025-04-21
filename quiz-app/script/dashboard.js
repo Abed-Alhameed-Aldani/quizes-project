1d30bc1a0a484adb9d29dac2752ec7097b1ac65f
Abed-Aldani <abedaldani2@gmail.com>
2025-04-21 21:24:30 +0300

    Getting the data of all users and quizzes

--- quiz-app/script/dashboard.js
+++ quiz-app/script/dashboard.js
@@ -4,3 +4,9 @@ if (currentUser !== "admin@quiz.com") {
   alert("Access denied. Admins only.");
   window.location.href = "index.html";
 }
+
+
+const users = JSON.parse(localStorage.getItem("users")) || [];
+const quizzes = JSON.parse(localStorage.getItem("quizzes")) || [];
+
+const userTable = document.getElementById("userTable");
